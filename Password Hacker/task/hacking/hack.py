b143d42d35d572c119b09e17c047d7ac35d97404
James Ng <james.ng@projapps.com>
2020-05-20 21:03:58 +0800

    Complete Stage #1

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,1 +1,17 @@
 # write your code here
+import argparse
+import socket
+
+parser = argparse.ArgumentParser()
+parser.add_argument('host', help="server hostname")
+parser.add_argument('port', help="server port number", type=int)
+parser.add_argument('password', help="server password")
+args = parser.parse_args()
+
+with socket.socket() as sock:
+    address = (args.host, args.port)
+    sock.connect(address)
+    data = args.password.encode()
+    sock.send(data)
+    response = sock.recv(1024)
+    print(response.decode())
